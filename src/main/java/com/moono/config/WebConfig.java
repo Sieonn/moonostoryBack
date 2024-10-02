ae1836c099596739e1ed12b7e4d187d469c31508
임시언 <145124127+Sieonn@users.noreply.github.com>
2024-10-02 23:27:56 +0900

    Update WebConfig.java

--- src/main/java/com/moono/config/WebConfig.java
+++ src/main/java/com/moono/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings (CorsRegistry registry) {
 		registry.addMapping("/*") //모든 경로에 대해
-		.allowedOrigins("http://localhost:3000") //허용할 도메인
+		.allowedOrigins("https://moono-stroy-eons-projects-65a9403c.vercel.app") //허용할 도메인
 		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") //허용할 HTTP 메서드
 		.allowedHeaders("*") //허용할 헤더
 		.allowCredentials(true); //인증정보 허용
