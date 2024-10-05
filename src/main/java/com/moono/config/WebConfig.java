dc0a6fe37b00a4c56c84d4a6a1ac85eb9c3d7fa9
임시언 <145124127+Sieonn@users.noreply.github.com>
2024-10-05 23:42:22 +0900

    Update WebConfig.java

--- src/main/java/com/moono/config/WebConfig.java
+++ src/main/java/com/moono/config/WebConfig.java
@@ -1,5 +1,5 @@
 package com.moono.config;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings (CorsRegistry registry) {
-		registry.addMapping("/*") //모든 경로에 대해
+		registry.addMapping("/api/**") //모든 경로에 대해
 		.allowedOrigins("https://moono-stroy.vercel.app") //허용할 도메인
 		.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") //허용할 HTTP 메서드
 		.allowedHeaders("*") //허용할 헤더
