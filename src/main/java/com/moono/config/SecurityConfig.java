55c359ea758189195c6df93ee0aee92f5c3a15fa
임시언 <145124127+Sieonn@users.noreply.github.com>
2024-10-06 00:07:38 +0900

    Update SecurityConfig.java

--- src/main/java/com/moono/config/SecurityConfig.java
+++ src/main/java/com/moono/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package com.moono.config;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
