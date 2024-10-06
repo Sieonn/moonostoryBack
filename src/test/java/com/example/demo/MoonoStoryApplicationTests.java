930d17ac331a53f2d65882f5c9e362016f07e8bb
sieon <sieon.eve@gmail.com>
2024-10-06 19:15:16 +0900

    feat: mvn test 어노테이션 추가

--- src/test/java/com/example/demo/MoonoStoryApplicationTests.java
+++ src/test/java/com/example/demo/MoonoStoryApplicationTests.java
@@ -3,7 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.moono.MoonoStoryApplication;
+
+@SpringBootTest(classes = MoonoStoryApplication.class)  // 여기에 실제 애플리케이션 클래스 추가
 class MoonoStoryApplicationTests {
 
 	@Test
