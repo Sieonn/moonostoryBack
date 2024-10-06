6f38b32a5ed6efdfe116cdf970625d607fcc770d
sieon <sieon.eve@gmail.com>
2024-10-06 19:10:26 +0900

    SecurityConfig.java 수정

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/MoonoStory.jar
--- src/main/java/com/moono/config/SecurityConfig.java
+++ src/main/java/com/moono/config/SecurityConfig.java
@@ -1,21 +1,23 @@
 package com.moono.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors() // CORS 설정 허용
+            .cors() // CORS 설정
             .and()
             .csrf().disable() // CSRF 비활성화 (필요시)
-            .authorizeRequests()
-            .antMatchers("/api/user/login", "/api/user/register", "/api/user/check-id").permitAll() // 특정 경로 허용
+            .authorizeHttpRequests()
+            .requestMatchers("/api/user/login", "/api/user/register", "/api/user/check-id").permitAll() // 특정 경로 허용
             .anyRequest().authenticated(); // 그 외 경로는 인증 필요
 
         return http.build();
