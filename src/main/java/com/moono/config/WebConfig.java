0c1ec44834516bf3293c222a93db5c0bc7ca7dda
sieon <sieon.eve@gmail.com>
2024-10-07 10:21:11 +0900

    다시 시도

--- src/main/java/com/moono/config/WebConfig.java
+++ src/main/java/com/moono/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings (CorsRegistry registry) {
-	    registry.addMapping("/**")
+	    registry.addMapping("/api/**")
 	    .allowedOriginPatterns("https://moono-stroy.vercel.app")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("Authorization", "Content-Type")
