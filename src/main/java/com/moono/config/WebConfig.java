a60873e06dc9ca954279e4fceefcdc99e670bf97
sieon <sieon.eve@gmail.com>
2024-10-07 11:16:17 +0900

    update

--- src/main/java/com/moono/config/WebConfig.java
+++ src/main/java/com/moono/config/WebConfig.java
@@ -9,10 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://moono-stroy.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+            .allowedOrigins("https://moono-stroy.vercel.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("Authorization", "Content-Type")
+            .allowCredentials(true);
     }
-
 }
